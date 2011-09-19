f12e9536efba4dedc7a34e853d18aa24afc1ca51
roTT <rotten@deathstar.(none)>
2011-09-19 11:58:32 -0300

    jvs init cleanup

--- buildgalaxysB.sh
+++ buildgalaxysB.sh
@@ -1,5 +1,5 @@
-echo "Using JV3 Precompiled modules (I9000B)"
-cp usr/prebuilt_ko_JV3.zip usr/prebuilt_ko.zip
+echo "Using JVS Precompiled modules (I9000B)"
+cp usr/prebuilt_ko_JVS.zip usr/prebuilt_ko.zip
 ./build.sh aries_ltb_talon_defconfig
 echo "Restoring JVR Precompiled modules (I9000)"
 cp usr/prebuilt_ko_JVR.zip usr/prebuilt_ko.zip
